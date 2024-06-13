39587bf97d1d351dd843f8eba32a36163ce9d494
SandeepProjects <Bagosandeep@gmail.com>
2024-06-13 12:54:39 +0200

    up

---
+++ my-app/src/Components/Header.js
@@ -0,0 +1,17 @@
+// src/components/Header.js
+import React from 'react';
+import './Header.css';
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Extramus</h1>
+      <div className="header-right">
+        <span>28 August 2024</span>
+        <span> | </span>
+        <span>Ebube Samuel</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
