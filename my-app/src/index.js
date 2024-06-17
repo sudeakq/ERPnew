c12d9e29d9d1b3bf5536dd422b2a22747c4a6c7c
Striligkas Manos <dpsd18108@aegean.gr>
2024-06-17 11:21:30 +0200

    upd

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,39 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-<<<<<<< HEAD
-
-=======
 // Uncomment and import the necessary components as needed
 // import WhatsNewClick from './WhatsNewClick';
->>>>>>> 71c755c939ea74f793ad5607269dd22c33547cef
 // import Intern from './Intern';
 // import DailyReminders from './Events';
 // import Dashboard from './Dashboard';
 // import UpcomingBirthdays from './UpcomingBirthdays';
 // import ScheduleView from './Schedule-2';
 // import Alumni from './Alumni';
-<<<<<<< HEAD
- //import House from './Houses';
- import Tmimata from './Tmimata';
-
-
-
-
-
-=======
 // import House from './Houses';
 import Bills from './Bills';
 // import Depar from './Depar';  // Fixed the path
 // import AllHous from './AllHouse';
->>>>>>> 71c755c939ea74f793ad5607269dd22c33547cef
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-    < Tmimata/>
-=======
     {/* Uncomment the component you want to render */}
     {/* <WhatsNewClick /> */}
     {/* <Intern /> */}
@@ -48,7 +31,6 @@ root.render(
     <Bills />
     {/* <Depar /> */}
     {/* <AllHous /> */}
->>>>>>> 71c755c939ea74f793ad5607269dd22c33547cef
   </React.StrictMode>
 );
 
