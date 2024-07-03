212b47fe46ea3841d5f75fa10ee9dc6285d11243
Deniz Yetim <denizyetim@hotmail.com>
2024-07-03 14:34:08 +0300

    index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,29 +4,28 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import UpcomingBirthdays from './UpcomingBirthdays';
 
-
 // import ScheduleView from './Schedule-2';
 // import DailyReminders from './Events';
 // import Schedule from './Schedule';
 // import ApplicantsForm from './ApllicantForm';
 // import ApplicantsList from './Components/ApllicantsList';
 // import ApplicantsForms from './Components/ApllicantForms';
-//import WhatsNew from './WhatsNew';
+// import WhatsNew from './WhatsNew';
 
 // Uncomment and import the necessary components as needed
-/* import WhatsNewClick from './WhatsNewClick'; */
-//import Intern from './Intern'; 
-//  import DailyReminders from './Events';
- // import Alumni from './Dashboard';
+// import WhatsNewClick from './WhatsNewClick'; */
+// import Intern from './Intern'; 
+// import DailyReminders from './Events';
+// import Alumni from './Dashboard';
 // import UpcomingBirthdays from './UpcomingBirthdays';
 // import ScheduleView from './Schedule-2';
-/* import Alumni from './Alumni'; */
-//  import House from './Houses';
- /* import Bills from './Bills'; */
+// import Alumni from './Alumni'; 
+// import House from './Houses';
+// import Bills from './Bills'; 
 // import Depar from './Depar';  // Fixed the path
 // import AllHous from './AllHouse';
 // import Applicants from './Applicants';
-import StartPage from './StartPage'; 
+// import StartPage from './StartPage'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -35,16 +34,13 @@ root.render(
     {/* <WhatsNewClick />  */}
     {/* <Intern /> */}
     {/* <DailyReminders /> */}
-    <StartPage />   
     {/* <UpcomingBirthdays /> */}
     {/* <ScheduleView /> */}
-
- {/*    <Alumni /> */}
+    {/* <Alumni /> */}
     {/* <House /> */}
     {/*  <StartPage />  */}  {/* Done */}
     {/* <ApplicantsList /> */}
-    {/* <Depar /> */}
-    
+    {/* <Depar /> */} 
   </React.StrictMode> 
 );
 
