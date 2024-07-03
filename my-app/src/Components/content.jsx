58eaae6e79b96e6d9ce7707c726843d064ed511a
sudeakq <sudeakq@gmail.com>
2024-07-03 14:43:48 +0300

    update the dashboard content

--- my-app/src/Components/content.jsx
+++ my-app/src/Components/content.jsx
@@ -4,13 +4,14 @@ import './content.css';
 import DashCard from './DashCard';
 import { FaArrowRightLong } from "react-icons/fa6";
 import WeeklyScheduleCard from './WeeklyScheduleCard';
+import DailyReminderCard from './DailyReminderCard';
 
 const Content = () => {
   return (
     <div className='content-container'>
       <div className="content-container-left">
 
-        {/* What's New? Section */}
+        {/* What's New? Section starts*/}
         <div className='content-container-top'>
           <h4>What's New?</h4>
           <div className='view-all'>
@@ -21,7 +22,9 @@ const Content = () => {
         <DashCard day="Fri" number={15} description="Project Management" backgroundColor="rgba(115, 86, 255, 1)" />
         <DashCard day="Sat" number={16} description="New Arrival" backgroundColor="rgba(78, 151, 232, 1)" />
         <DashCard day="Sun" number={17} description="Interview" backgroundColor="rgba(76, 139, 210, 1)" />
-        {/* Weekly Schedule Section */}
+        {/* What's New? Section ends*/}
+
+        {/* Weekly Schedule Section starts*/}
         <div className='content-container-top' style={{ marginTop: '30px' }}>
           <h4>Weekly Schedule</h4>
           <div className='view-all'>
@@ -29,10 +32,16 @@ const Content = () => {
           </div>
         </div>
         <WeeklyScheduleCard></WeeklyScheduleCard>
-
-
-
-
+        {/* Weekly Schedule Section ends*/}
+      </div>
+      <div className='content-container-right'>
+        <div className='content-container-top'>
+          <h4>Daily Reminder</h4>
+          <div className='view-all'>
+            <a href="./" >View All (12)</a>
+          </div>
+        </div>
+        <DailyReminderCard />
       </div>
     </div>
   );
