9e35d8bb3101a80f2ef4d8b431d59fbfbfb27a42
Hidayet Çiftçi <148482270+heyedos@users.noreply.github.com>
2024-07-03 15:09:26 +0300

    changeindex

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -16,6 +16,7 @@ import reportWebVitals from './reportWebVitals';
 // Uncomment and import the necessary components as needed
 /* import WhatsNewClick from './WhatsNewClick'; */
 import Intern from './Intern'; //
+import Intern from './Intern'; //
 //  import DailyReminders from './Events';
  // import Alumni from './Dashboard';
 // import UpcomingBirthdays from './UpcomingBirthdays';
@@ -23,7 +24,7 @@ import Intern from './Intern'; //
 /* import Alumni from './Alumni'; */
 //  import House from './Houses';
 // import Bills from './Bills'; //
-// import ForgotPassword from './UserSıde/ForgotPassword'
+//import ForgotPassword from './UserSıde/ForgotPassword'
 // import Depar from './Depar';  // Fixed the path
 // import AllHous from './AllHouse';
 // import Applicants from './Applicants';
@@ -38,8 +39,8 @@ root.render(
     {/* <DailyReminders /> */}
     {/* <UpcomingBirthdays /> */}
     {/* <ScheduleView /> */}
-    {/* <Alumni /> */}
-    <Intern />
+<Intern />
+ {/*    <Alumni /> */}
     {/* <House /> */}
     {/* <StartPage />   */}   {/* Done */}
     {/* <ApplicantsList /> */}
