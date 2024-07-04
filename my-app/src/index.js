d25ae4706ae8039d3865f3c4c88d46b81be6f2f2
SandeepProjects <Bagosandeep@gmail.com>
2024-07-04 10:40:17 +0200

    change

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,34 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
-/* import UpcomingBirthdays from './UpcomingBirthdays'; */
-=======
 // import UpcomingBirthdays from './UpcomingBirthdays';
 
->>>>>>> 49e2e93dd5f2c3e4fd4eea19a434c85ef1462fbd
 
 // import ScheduleView from './Schedule-2';
 // import DailyReminders from './Events';
 // import Schedule from './Schedule';
 // import ApplicantsForm from './ApllicantForm';
-// import ApplicantsList from './Components/ApllicantsList';
+import ApplicantsList from './Components/ApllicantsList';
 // import ApplicantsForms from './Components/ApllicantForms';
 // import WhatsNew from './WhatsNew';
 
 // Uncomment and import the necessary components as needed
-<<<<<<< HEAD
-// import WhatsNewClick from './WhatsNewClick'; 
-import Intern from './Intern'; 
-// import DailyReminders from './Events';
-// import Alumni from './Dashboard';
-// import UpcomingBirthdays from './UpcomingBirthdays';
-// import ScheduleView from './Schedule-2';
-// import Alumni from './Alumni';
-// import House from './Houses';
-// import Bills from './Bills'; 
-// import ForgotPassword from './UserSıde/ForgotPassword'
-=======
 /* import WhatsNewClick from './WhatsNewClick'; */
 // import Intern from './Intern'; //
 //  import DailyReminders from './Events';
@@ -39,8 +23,7 @@ import Intern from './Intern';
 /* import Alumni from './Alumni'; */
 //  import House from './Houses';
 // import Bills from './Bills'; //
-import ForgotPassword from './UserSıde/ForgotPassword'
->>>>>>> 49e2e93dd5f2c3e4fd4eea19a434c85ef1462fbd
+// import ForgotPassword from './UserSıde/ForgotPassword'
 // import Depar from './Depar';  // Fixed the path
 // import AllHous from './AllHouse';
 // import Applicants from './Applicants';
@@ -56,10 +39,10 @@ root.render(
     {/* <UpcomingBirthdays /> */}
     {/* <ScheduleView /> */}
     {/* <Alumni /> */}
-    <Intern />
+    {/* <Intern /> */}
     {/* <House /> */}
     {/* <StartPage />   */}   {/* Done */}
-    {/* <ApplicantsList /> */}
+    <ApplicantsList />
     {/* <Depar /> */} 
   </React.StrictMode> 
 );
