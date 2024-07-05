758afd134f4b8e464663419a9bc83d5cb6d9cf71
Striligkas Manos <dpsd18108@aegean.gr>
2024-07-05 10:08:15 +0200

    API FOR STUDENT AND SANCTUM FOR SAFETY

--- my-app/backend/routes/api.php
+++ my-app/backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,11 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('users',[UserController::class,'index']);
-Route::get('users/{id}',[UserController::class,'show']);
 
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/test-cors', function (Request $request) {
-    return response()->json(['message' => 'CORS is working!']);
-});
+Route::apiResource('/student', EmployeeController::class);
