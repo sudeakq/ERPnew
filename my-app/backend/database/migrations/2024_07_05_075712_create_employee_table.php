4e795be8209b915ea251541db7539c3e725d8b62
Striligkas Manos <dpsd18108@aegean.gr>
2024-07-05 10:00:28 +0200

    Details of EmployeeController

--- my-app/backend/database/migrations/2024_07_05_075712_create_employee_table.php
+++ my-app/backend/database/migrations/2024_07_05_075712_create_employee_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('employee', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
