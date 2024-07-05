097f989bddac19b21d484cd516e9b3d97a5ecfed
Striligkas Manos <dpsd18108@aegean.gr>
2024-07-05 10:06:09 +0200

    details of model

--- my-app/backend/app/Models/Employee.php
+++ my-app/backend/app/Models/Employee.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
-{
+{   protected $table = 'employee';
+    protected $primaryKey = 'id';
+     protected $fillable = [
+          'name',
+          'address',
+          'phone',
+      ];
     use HasFactory;
 }
