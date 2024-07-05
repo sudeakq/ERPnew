be4357bd6a683e38d191a4cb79fd126dedbf9664
Striligkas Manos <dpsd18108@aegean.gr>
2024-07-05 10:12:10 +0200

    EMPLOYEE CONTROLLER

--- my-app/backend/app/Http/Controllers/EmployeeController.php
+++ my-app/backend/app/Http/Controllers/EmployeeController.php
@@ -1,48 +1,41 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
+use App\Models\Employee;
 class EmployeeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    protected $employee;
+    public function __construct(){
+        $this->employee = new Employee();
+
+    }
     public function index()
     {
-        //
+        return $this->employee->all();
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+     return $this->employee->create($request->all());
+
+
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+     $student = $this->employee->find($id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+         $employee = $this->employee->find($id);
+         $employee->update($request->all());
+         return $employee;
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+     $employee = $this->employee->find($id);
+    return $employee->delete();
     }
 }
