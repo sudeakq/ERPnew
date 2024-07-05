8332c35e5844b5f344d28c49bc051118b28535f5
Deniz Yetim <denizyetim@hotmail.com>
2024-07-05 16:55:01 +0300

    Done

--- my-app/src/Components/Departments.jsx
+++ my-app/src/Components/Departments.jsx
@@ -1,46 +1,50 @@
 import React from 'react';
 import { DepartmantsContainer } from './Departments.style';
+import { useNavigate } from 'react-router-dom';
 
 function Departments() {
+
+  const navigate = useNavigate();
+
   return (
     <DepartmantsContainer>
-        <div className="title">
+        <div className="title" onClick={()=>navigate('workers')} >
           <h4>MEET THE TEAMS</h4>
         </div>
         <div className="grid">
-          <div className="card">    
+          <div className="card" onClick={()=>navigate('/workers')} >    
             <h2>HR</h2>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-          <div className="card">    
+          <div className="card" onClick={()=>navigate('/workers')} >    
             <h2>HR</h2>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-          <div className="card">    
+          <div className="card" onClick={()=>navigate('/workers')} >    
             <h2>HR</h2>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-          <div className="card">    
+          <div className="card" onClick={()=>navigate('/workers')} >    
             <h2>HR</h2>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-          <div className="card">    
+          <div className="card" onClick={()=>navigate('/workers')} >    
             <h2>HR</h2>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-          <div className="card">    
+          <div className="card" onClick={()=>navigate('/workers')} >    
             <h2>HR</h2>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-          <div className="card">    
+          <div className="card" onClick={()=>navigate('/workers')} >    
             <h2>HR</h2>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-          <div className="card">    
+          <div className="card" onClick={()=>navigate('/workers')} >    
             <h2>HR</h2>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-          <div className="card">    
+          <div className="card" onClick={()=>navigate('/workers')} >    
             <h2>HR</h2>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
