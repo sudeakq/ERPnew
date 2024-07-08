c979d02006accfcca846848783475f31f2125080
sudeakq <sudeakq@gmail.com>
2024-07-08 14:13:04 +0300

    problem fix

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -10,7 +10,7 @@ import Events from './Events';
 import Intern from './Intern';
 import Dashboard from './Dashboard';
 import UpcomingBirthdays from './UpcomingBirthdays';
-import UpcomingArrival from './UpcomingArrivals';
+import UpcomingArrivals from './UpcomingArrivals';
 import ForgotPassword from './UserSıde/ForgotPassword'
 import ScheduleView from './Schedule-2';
 import Alumni from './Alumni';
@@ -45,7 +45,7 @@ root.render(
         <Route path='/bills' element={<Bills />} />
         <Route path='/houses' element={<AllHous />} />
         <Route path='/applicants' element={<Applicants />} />
-        <Route path='/arrivals' element={<UpcomingArrival />} />
+        <Route path='/arrivals' element={<UpcomingArrivals />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
