090ed16833dbe5c94043f9e512f8b178579544e1
sudeakq <sudeakq@gmail.com>
2024-07-08 09:33:28 +0300

    Router changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,66 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
-/* import UpcomingBirthdays from './UpcomingBirthdays'; */
-=======
-// import UpcomingBirthdays from './UpcomingBirthdays';
 
->>>>>>> 49e2e93dd5f2c3e4fd4eea19a434c85ef1462fbd
-
-// import ScheduleView from './Schedule-2';
-// import DailyReminders from './Events';
-// import Schedule from './Schedule';
-// import ApplicantsForm from './ApllicantForm';
-// import ApplicantsList from './Components/ApllicantsList';
-// import ApplicantsForms from './Components/ApllicantForms';
-// import WhatsNew from './WhatsNew';
-
-// Uncomment and import the necessary components as needed
-<<<<<<< HEAD
-// import WhatsNewClick from './WhatsNewClick'; 
-import Intern from './Intern'; 
-// import DailyReminders from './Events';
-// import Alumni from './Dashboard';
-// import UpcomingBirthdays from './UpcomingBirthdays';
-// import ScheduleView from './Schedule-2';
-// import Alumni from './Alumni';
-// import House from './Houses';
-// import Bills from './Bills'; 
-// import ForgotPassword from './UserSıde/ForgotPassword'
-=======
-/* import WhatsNewClick from './WhatsNewClick'; */
-// import Intern from './Intern'; //
-//  import DailyReminders from './Events';
- // import Alumni from './Dashboard';
-// import UpcomingBirthdays from './UpcomingBirthdays';
-// import ScheduleView from './Schedule-2';
-/* import Alumni from './Alumni'; */
-//  import House from './Houses';
-// import Bills from './Bills'; //
+import StartPage from './StartPage';
+import WhatsNewClick from './WhatsNewClick';
+import DailyReminders from './Events';
+import Intern from './Intern';
+import Dashboard from './Dashboard';
+import UpcomingBirthdays from './UpcomingBirthdays';
 import ForgotPassword from './UserSıde/ForgotPassword'
->>>>>>> 49e2e93dd5f2c3e4fd4eea19a434c85ef1462fbd
-// import Depar from './Depar';  // Fixed the path
-// import AllHous from './AllHouse';
-// import Applicants from './Applicants';
-// import StartPage from './StartPage';
+import ScheduleView from './Schedule-2';
+import Alumni from './Alumni';
+import Bills from './Bills';
+import AllHous from './AllHouse';
+import Applicants from './Applicants';
+import Departments from './Departments';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    {/* Uncomment the component you want to render */}
-    {/* <WhatsNewClick /> */} {/* done */} 
-    {/* <ForgotPassword />   */}
-    {/* <DailyReminders /> */}
-    {/* <UpcomingBirthdays /> */}
-    {/* <ScheduleView /> */}
-    {/* <Alumni /> */}
-    <Intern />
-    {/* <House /> */}
-    {/* <StartPage />   */}   {/* Done */}
-    {/* <ApplicantsList /> */}
-    {/* <Depar /> */} 
-  </React.StrictMode> 
+    <BrowserRouter>
+      <Routes  >
+        <Route path='/' element={<StartPage />} />
+        <Route path='/departments' element={<Departments />} />
+        <Route path='/news' element={<WhatsNewClick />} />
+        <Route path='/interns' element={<Intern />} />
+        <Route path='/reminder' element={<DailyReminders />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/birthdays' element={<UpcomingBirthdays />} />
+        <Route path='/forgotPassword' element={<ForgotPassword />} /> {/* type the "path" on url and you'll go to the page */}
+        <Route path='/schedule' element={<ScheduleView />} />
+        <Route path='/alumni' element={<Alumni />} />
+        <Route path='/bills' element={<Bills />} />
+        <Route path='/houses' element={<AllHous />} />
+        <Route path='/applicants' element={<Applicants />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
