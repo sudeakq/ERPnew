c026aac79b2bbcf3853d9a8cd839f319ae362a92
Deniz Yetim <denizyetim@hotmail.com>
2024-07-11 13:45:19 +0300

    users migration

--- my-app/backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ my-app/backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Student;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,10 +15,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
+            $table->string('phone_number');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('usertypes_id');
+            $table->foreignIdFor(Student::class)->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
