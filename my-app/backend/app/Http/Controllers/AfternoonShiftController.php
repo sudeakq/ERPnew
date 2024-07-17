8f91d1b65e6d15b1404c6939d51ed2087911eab5
Deniz Yetim <denizyetim@hotmail.com>
2024-07-17 13:39:30 +0300

    Morning/Afternoon Shift

---
+++ my-app/backend/app/Http/Controllers/AfternoonShiftController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AfternoonShift;
+use Illuminate\Http\Request;
+
+class AfternoonShiftController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AfternoonShift $afternoonShift)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AfternoonShift $afternoonShift)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AfternoonShift $afternoonShift)
+    {
+        //
+    }
+}
